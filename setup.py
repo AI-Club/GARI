f3eccabb6cf2ba2e930cb447e93b1653c619c61e
Anthony Johnson <aj@ohess.org>
2015-09-07 15:22:08 -0700

    Import boilerplate for module development

---
+++ setup.py
@@ -0,0 +1,18 @@
+'''
+Good Agent-based Romcom Intelligence
+'''
+
+import sys
+from setuptools import setup, find_packages
+
+
+setup(
+    name='GARI',
+    version='0.1.1',
+    license='BSD',
+    description='',
+    packages=find_packages(),
+    long_description=sys.modules[__name__].__doc__,
+    include_package_data=True,
+    install_requires=[],
+)
