a28aa3c3575b6c469f327a4803a350091907902c
Anthony Johnson <aj@ohess.org>
2015-09-07 17:04:32 -0700

    More boilerplate

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gari',
-    version='0.1.1',
+    version='0.1',
     license='BSD',
     description='',
     packages=find_packages(),
